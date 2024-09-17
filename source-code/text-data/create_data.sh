dec987176d0c13c2bd3b9c7e1f1dd01c89e5c8f7
Geert Jan Bex <geertjan.bex@uhasselt.be>
2024-09-17 13:59:23 +0200

    Add label creation

--- source-code/text-data/create_data.sh
+++ source-code/text-data/create_data.sh
@@ -32,18 +32,30 @@ fi
 mkdir "$1"
 
 # Create the text files
+(2>&1 echo "Creating $2 text files with max word length $3")
 for i in $(seq 1 $2); do
     python ../data-generation/create_text_data.py \
         --words=1 \
         --max-word-length=$3 \
+        --char-set=ACGT \
         --output-file "$1"/seq$(printf "%06d" $i).txt
 done
 
 # Create the TextIndex text and index file
+(2>&1 echo "Creating text index file")
 python index_text_files.py "$1/seq*.txt" "$1.txt"
 
 # Create the zip file
+(2>&1 echo "Creating zip file")
 zip -q -j "$1.zip" "$1"/seq*.txt
 
 # Create the tar file
+(2>&1 echo "Creating tar file")
 tar -cf "$1.tar" "$1"/seq*.txt
+
+# Create the CSV labels file
+(2>&1 echo "Creating labels file")
+python ../data-generation/create_labels.py "$1/seq*.txt" --types int:2 str:10 --output "$1_labels.csv"
+
+# Done
+(2>&1 echo "Done")
