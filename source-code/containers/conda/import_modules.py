9ff632baa3be7bc6c567777c3aef7470041c9dfe
Geert Jan Bex <geertjan.bex@uhasselt.be>
2024-10-02 16:42:00 +0200

    Add more modules

---
+++ source-code/containers/conda/import_modules.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import argparse
+import collections
+import configparser
+import datetime
+import itertools
+import time
+
+import numpy as np
+
+import pandas as pd
+
+from scipy import stats, integrate, optimize, signal
+
+import matplotlib.pyplot as plt
+
+from sklearn.base import TransformerMixin
+from sklearn.compose import ColumnTransformer
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.manifold import TSNE
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
+
+from tensorflow import keras
+from keras.datasets import imdb
+from keras.preprocessing import sequence
+
+import torch
+from torch.func import functional_call, grad, vjp, vmap
+
+
+if __name__ == '__main__':
+    print('Importing modules is successful.')
